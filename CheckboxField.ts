e4f2b79515a8f29372d12833268ebc3b92a93d22
stephan <m6ahenina@gmail.com>
2020-08-21 14:02:01 +0300

    initial commit

---
+++ CheckboxField.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CheckboxField = ({ input, label }) => (
+  <div>
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={input.value ? true : false}
+          onChange={input.onChange}
+        />
+      }
+      label={label}
+    />
+  </div>
+);
+
+export default CheckboxField;
---
+++ TextField.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TextField = ({
+    label,
+    input,
+    meta: { touched, invalid, error },
+    ...custom
+}) => (
+    <TextField
+        label={label}
+        placeholder={label}
+        error={touched && invalid}
+        helperText={touched && error}
+        {...input}
+        {...custom}
+    />
+);
+
+export default TextField;
